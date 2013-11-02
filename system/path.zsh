6212ab33ddf6a3ad8f7eb16cd5bc8f2badcc1c43
Edvinas Arzatskis <arzatskis@gmail.com>
2013-11-02 14:37:26 +0200

    Add some sublime themes

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export PATH=/usr/local/share/npm/bin:$PATH #npm modules
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
